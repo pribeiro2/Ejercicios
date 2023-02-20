65a187185b0b376d4a27e01005f6787ab79845e6
Paulo Ribeiro <pribeiro2@gmail.com>
2023-02-20 19:35:41 -0300

    -commit

---
+++ Poo/Paulo-Poo/src/Poo/Guia7/Ejercicio12.java
@@ -0,0 +1,31 @@
+package Poo.Guia7;
+
+import java.util.Scanner;
+
+public class Ejercicio12 {
+
+    public static void main(String[] args) {
+        String cadena; 
+        int cuentaCorrectas = 0, cuentaIncorrectas = 0;
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Ingres cadena de 5 caracteres: ");
+         cadena = leer.nextLine(); 
+        while (!"&&&&&".equals(cadena))
+                {        
+            if (cadena.length() >= 5)
+             {
+                if ("O".equals(cadena.substring(0,1)) && "X".equals(cadena.substring(4,5)) && (cadena.length() == 5)) {
+                    cuentaCorrectas++;
+                } else {
+                    cuentaIncorrectas++;
+                }
+             } else { cuentaIncorrectas++;
+                    }
+                System.out.print("Ingres cadena de 5 caracteres: ");
+                cadena = leer.nextLine();     
+                }
+        System.out.println("Salio por recibir la cadena: " + cadena);
+        System.out.println("Cadenas Correctas : " + cuentaCorrectas);
+        System.out.println("Cadenas Inccorrectas : " + cuentaIncorrectas);
+    }
+}
